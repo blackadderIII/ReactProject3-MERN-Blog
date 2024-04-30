20398ba7854dea4f63c0c64e6fc3866f0e7f9063
blackadderIII <skobarnes1@gmail.com>
2024-04-30 21:56:42 +0000

    "Added about and contact routes; replaced 'Hello SKOB' with index page render"

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,20 @@ const router = express.Router();
 
 //  Test Server Request with Routes
 router.get('',(req,res)=>{
-    res.send("Hello SKOB")
+    const locals = {
+        title: "NodeJS Blog",
+        description:"Simple Blog created with NodeJs,Express & MongoDb."
+
+    }
+    res.render('index',{locals});
+});
+router.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+
+router.get('/contact',(req,res)=>{
+    res.render('contact');
 });
 
 module.exports = router;
