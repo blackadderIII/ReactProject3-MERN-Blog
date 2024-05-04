fc7431e257efe64a30cbe09726430c0f0d1d357d
blackadderIII <skobarnes1@gmail.com>
2024-05-04 12:18:16 +0000

    "Add get post by id route and view; update home route to include latest posts"
    This commit adds a new route for getting a specific post by its ID and a corresponding view for displaying the post. It also updates the home route

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-//  Test Server Request with Routes
+//  Get Home
 router.get("", async (req, res) => {
   try {
     const locals = {
@@ -46,6 +46,26 @@ router.get("", async (req, res) => {
 //         console.log(`Error Fetching From Database. ${error}`);
 //     }
 // });
+
+// Get Post:id
+router.get("/post/:id/", async (req, res) => {
+
+  try {
+    let slug = req.params.id;
+
+    
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/about", (req, res) => {
   res.render("about");
