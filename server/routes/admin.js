bbc7d5b5eeb5106634ab78396c2c9edd1a911d41
blackadderIII <skobarnes1@gmail.com>
2024-05-05 13:18:35 +0000

    .

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,6 +4,34 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 const adminLayout = '../views/layouts/admin';
 
+// Post
+// Admin - Check Login
+
+router.post("/admin", async (req, res) => {
+  try {
+      // Get user from the request body
+      const { username , password} = req.body;
+    
+    res.redirect('/admin')
+    // res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get("/admin", async (req, res) => {
   try {
     const locals = {
