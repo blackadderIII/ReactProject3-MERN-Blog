888993135f9fd056df49ce02d9a393e246e0c4f0
blackadderIII <skobarnes1@gmail.com>
2024-05-05 13:01:51 +0000

    "Updated admin route to include User model and adjust layout; removed admin index view"
    This commit updates the admin route in the server/routes/admin.js file to include the User model and adjust the layout.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,38 +1,20 @@
 const express = require("express");
 const router = express.Router();
-// const Post = require("../models/Post");
+const Post = require("../models/Post");
+const User = require("../models/User");
+const adminLayout = '../views/layouts/admin';
 
-
-const adminLayout = '../views/layouts/admin.ejs'
-
-
-router.get('/admin', async (req,res)=>{
-    
-    try {
-        
-        const locals = {title: 'NodeJs-Admin',
-            description:'Simple Blog created with NodeJs , Express & MongoDb.'            
+router.get("/admin", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs-Admin",
+      description: "Simple Blog created with NodeJs , Express & MongoDb.",
     };
 
-    res.render('admin/index',{locals, layout: adminLayout})
-} catch (error) {
-    console.log(error)
-}
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
