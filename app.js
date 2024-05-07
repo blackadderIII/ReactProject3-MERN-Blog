c7170336142b33f0298caa17afe062531b2c645b
blackadderIII <skobarnes1@gmail.com>
2024-05-07 20:14:14 +0000

    ..

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 require("dotenv").config();
 
 const express = require('express');
-const expressLayout = require('express-ejs-layouts')
+const expressLayout = require('express-ejs-layouts');
 const app = express();
 const PORT = 5000 || process.env.PORT;
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 
 // connect to database
 const connectDB = require('./server/config/db');
+const session = require("express-session");
 
 connectDB();
 
 // allows passing of data through forms
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store:MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    })
+    // cookie: {maxage: new Date (Date.now() + (3600000))}
+}
+
+))
 
 app.use(express.static('public'));
 
