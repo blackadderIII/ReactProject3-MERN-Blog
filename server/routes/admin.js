1e93cbdb4a7092d8307f224fb363b1087c2477e7
blackadderIII <skobarnes1@gmail.com>
2024-05-07 20:14:23 +0000

    ..

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,35 +2,47 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
-const adminLayout = '../views/layouts/admin';
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const adminLayout = "../views/layouts/admin";
 
 // Post
 // Admin - Check Login
 
 router.post("/admin", async (req, res) => {
   try {
-      // Get user from the request body
-      const { username , password} = req.body;
-    
-    res.redirect('/admin')
+    // Get user from the request body
+    const { username, password } = req.body;
+
+    res.redirect("/admin");
     // res.render('admin/index', { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
 });
+// Post
+// Admin - Register
 
+router.post("/register", async (req, res) => {
+  try {
+    // Get user from the request body
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-
-
-
-
-
-
-
-
-
-
-
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User Created", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "User already in use" });
+      }
+      res.status(500).json({message:'internal server error'})
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/admin", async (req, res) => {
   try {
@@ -39,7 +51,7 @@ router.get("/admin", async (req, res) => {
       description: "Simple Blog created with NodeJs , Express & MongoDb.",
     };
 
-    res.render('admin/index', { locals, layout: adminLayout });
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
