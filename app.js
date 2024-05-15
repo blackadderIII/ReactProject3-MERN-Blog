b334e01291a832dd5bf1c344717ced2ca59ffe52
blackadderIII <skobarnes1@gmail.com>
2024-05-15 17:10:58 +0000

    Update app.js and add admin delete post feature in admin.js
    In app.js, method-override middleware is added to handle PUT and DELETE requests. In admin.js, a new DELETE route is added.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override')
 const app = express();
 const PORT = 5000 || process.env.PORT;
 const cookieParser = require('cookie-parser');
@@ -16,7 +17,10 @@ connectDB();
 // allows passing of data through forms
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(methodOverride( '_method'));
+
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
