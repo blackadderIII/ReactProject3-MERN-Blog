2193aa7855ca0010312bd4a6802a205074aba037
blackadderIII <skobarnes1@gmail.com>
2024-05-15 16:43:57 +0000

    ..

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -126,6 +126,22 @@ router.get("/add-post", authMiddleware, async (req, res) => {
       console.log(error)
   }
 })
+ 
+// Edit Post
+router.get("/edit-post/:id", authMiddleware, async (req, res) => {
+  try{
+    const locals = {
+      title:'Edit Post',
+      description:'Edit Selected Post'
+    }
+    const data = await Post.findOne({_id: req.params.id });
+
+    res.render('admin/edit-post',{locals,data,layout:adminLayout})
+  }
+  catch(error){
+      console.log(error)
+  }
+})
 
 
 // Post Admin 
@@ -145,6 +161,24 @@ router.post("/add-post", authMiddleware, async (req, res) => {
       console.log(error)
   }
 })
+// Put Admin 
+// Edit Post
+
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
+  try{
+    await Post.findByIdAndUpdate(req.params.id,{
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now()
+    });
+
+    res.redirect(`/edit-post/${req.params.id}`)
+  }
+  catch(error){
+      console.log(error)
+  }
+})
+
 
 
 module.exports = router;
